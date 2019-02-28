a2c1d7309b33a5152b92de02083cc1dc72ba55e3
Vince Forgione <vforgione@uchicago.edu>
2019-02-28 13:37:10 -0600

    Locking api to use only json

--- queen_bee/settings.py
+++ queen_bee/settings.py
@@ -135,6 +135,13 @@ STATIC_URL = '/static/'
 # Rest framework configuration
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    ),
+
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 200,
     
